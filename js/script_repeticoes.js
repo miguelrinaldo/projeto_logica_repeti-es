2b30e76cc78be08f6a6a608487f6e86c39385f75
Miguel Rianldo <miguel.r.nascimento9@aluno.senai.br>
2026-06-15 21:50:11 -0300

    Adicionei vários comandos no js/ e HTML

--- js/script_repeticoes.js
+++ js/script_repeticoes.js
@@ -3,7 +3,7 @@ const divFor = document.querySelector(`#div-for`)
 
 
 //ESTRUTURAS DE REPETIÇÕES
-for(i = 0; i < 10; i+++){
+for(i = 0; i < 10; i++){
     console.log(i)
     divFor.innerHTML += `${i} <br>`
 }
@@ -15,7 +15,7 @@ const btnNum1 = document.querySelector(`#btn-num1`)
 const divResult = document.querySelector(`#result-cont-acum`)
 
 
-let cont = 0, contPar - 0, contImpar = 0
+let cont = 0, contPar = 0, contImpar = 0
 let acum = 0.0
 
 butNum1.addEventListener(`click`,(evt)=>{
@@ -48,14 +48,56 @@ const btnExibir = document.querySelector(`#btn-exibir`)
 const divResultFrase = document.querySelector(`#result-frase`)
 
 let contFrase = 0 
-let totalRepeticao = Number(inputRepeticao.value)
+let totalRepeticao = 0
 
 btnExibir.addEventListener('click', (evt)=>{
-    console.log(totalRepeticao  +'aqui')
+    totalRepeticao = Number(inputNumRepeticao.value)
+
     while(totalRepeticao > contFrase){
         divResultFrase.innerHTML = `${imputFrase.value} <br>`
     
-    contFrase++
+         contFrase++
     
     }
 })
+
+//COLEÇÃO DE DADOS - ARRAY
+
+transporte = ['Bicicleta', 'Carro', 'Moto', 'Ônibus', 'Avião', 'Tototo']
+
+for (i = 0; i < transporte.length; i++){
+    console.log(transporte[i])
+}
+
+//FOR IN
+const divForin = document.querySelector('#div-forin')
+
+for(let pos in transporte){
+    console.log(transporte[pos])
+divForin.innerHTML += `${transporte[pos]} <br>`
+}
+
+//FOR OF
+const divForOF = document.querySelector('#div-forof')
+
+for(let elem of transporte){
+    divForOF.innerHTML += `${elem} <br>` 
+}
+
+//COLEÇÃO DE PESSOAS - ARRAY DE OBJETO 
+const pessoas = [
+    {nome: 'Maria', idade: 18, renda: 15000 },
+    {nome:'Josefina' , idade: 23, renda: 1800 },
+    {nome: 'Chicó', idade: 32, renda: 1.50 },
+    {nome: 'João Grilo', idade: 34, renda: 2.20 },
+    
+]
+
+
+console.log('-----------LISTA POR FOR IN-----------')
+for(let pessoa in pessoas){
+    console.log(pessoas.nome, pessoa.idade, pessoa.renda)
+}
+
+//FOREACH
+const divForeach = document.querySelector('#div-foreach')
